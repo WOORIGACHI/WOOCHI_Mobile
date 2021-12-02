67fd266ee1a3663f23cc979b157e6e577ed535e4
sowon <sowonkim177@gmail.com>
2021-12-03 00:39:44 +0900

    test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>되는지만 확인~~</Text>
       <StatusBar style="auto" />
     </View>
   );
